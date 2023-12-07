b328964effd663ee6f06f6183c602e1d635e645f
Denis <den.ostroukhov.02@mail.ru>
2023-12-07 21:19:08 +0700

    Легковес

--- OOP_LastChance/OOP_LastChance.cpp
+++ OOP_LastChance/OOP_LastChance.cpp
@@ -1,24 +1,7 @@
 ﻿#include "Child.h"
-#include "Bridge.h"
+#include "Flyweight.h"
 
 void TestBridgePattern() {
-    // Создаем конкретные реализации действий и черт
-    ICountingAction* canCount = new CanCounting();
-    ICharacterFeatures* evil = new Evil();
-
-    // Создаем абстракции с конкретными реализациями
-    ChildAction* countingAction = new CountingChildAction(canCount);
-    ChildFeatures* characterFeatures = new CharacterChildFeatures(evil);
-
-    // Выполняем действия и отображаем черты
-    countingAction->performAction();
-    characterFeatures->displayFeatures();
-
-    // Освобождаем память
-    delete canCount;
-    delete evil;
-    delete countingAction;
-    delete characterFeatures;
 
 }
 
@@ -27,23 +10,12 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-	//Текст капсом для титульного листа
-	Text* txtForTitle = new Text(new ForTitleList());
-	cout << "Текст капсом для титульного листа: " << endl;
-	txtForTitle->WriteYourText("Walter");
-	//Текст не капсом для внутреннего листа
-	Text* txtForSecond = new Text(new ForSecondList());
-	cout << endl << endl << "Текст не капсом для внутреннего листа: " << endl;
-	txtForSecond->WriteYourText("Walter");
-	//Обведённый текст для титульного листа
-	Table* tblForTitle = new Table(new ForTitleList());
-	cout << endl << endl << "Обведённый текст для титульного листа: " << endl;
-	tblForTitle->WriteYourText("Walter");
-	//Обведённый текст для внутреннего листа
-	Table* tblForSecond = new Table(new ForSecondList());
-	cout << endl << endl << "Обведённый текст для внутреннего листа: " << endl;
-	tblForSecond->WriteYourText("Walter");
-	delete tblForTitle, tblForSecond;
-	delete txtForTitle, txtForSecond;
-	return 0;
+    srand(time(NULL));
+    PensilCase* pencilCase = new PensilCase();
+    const int CodeRange = 39; //Диапазон кода
+    for (int i = 0; i < CodeRange; i++) {
+        pencilCase->getTools(randomRange(10, CodeRange))->display();
+    }
+    return 0;
+    delete pencilCase;
 }
