8f263a9b16f86f4db6f71c1f6b9c499d350d12d9
Denis <den.ostroukhov.02@mail.ru>
2023-12-08 05:38:40 +0700

    Фикс

--- OOP_LastChance/Flyweight.h
+++ OOP_LastChance/Flyweight.h
@@ -7,24 +7,24 @@ using namespace std;
 //Предметы для рисования
 class ObjectsForDriwing {
 protected:
-	int Code; //Код сигнала
+	int Colour; //Код сигнала
 public:
-	ObjectsForDriwing(int Code) { this->Code = Code; }
+	ObjectsForDriwing(int Colour) { this->Colour = Colour; }
 	virtual void display() = 0;
 };
 class Pencil : public ObjectsForDriwing {
 public:
-	Pencil(int Code) : ObjectsForDriwing(Code) {}
+	Pencil(int Colour) : ObjectsForDriwing(Colour) {}
 	virtual void display() { cout << "Карандаш" << endl; }
 };
 class Crayon : public ObjectsForDriwing {
 public:
-	Crayon(int Code) : ObjectsForDriwing(Code) {}
+	Crayon(int Colour) : ObjectsForDriwing(Colour) {}
 	virtual void display() { cout << "Фломастер" << endl; }
 };
 class Paint : public ObjectsForDriwing {
 public:
-	Paint(int Code) : ObjectsForDriwing(Code) {}
+	Paint(int Colour) : ObjectsForDriwing(Colour) {}
 	virtual void display() { cout << "Краска" << endl; }
 };
 //Компьютер - фабрика сигналов
@@ -34,10 +34,10 @@ private:
 	ArrayToolsForDraw mArrayToolsForDraw;
 public:
 	ObjectsForDriwing* getTools(int inCode) {
-		cout << inCode % 10 << " grade - ";
+		cout << inCode % 10 << " цвет ";
 		ArrayToolsForDraw::iterator it = mArrayToolsForDraw.find(inCode);
 		if (mArrayToolsForDraw.end() == it) {//Если не найден, то создаем новый
-			cout << "(new) - ";
+			cout << "новый - ";
 			ObjectsForDriwing* f;
 			if (inCode / 10 == 1)
 				f = new Pencil(inCode);
@@ -51,7 +51,7 @@ public:
 			return f;
 		}
 		else {//Если найден, то возвращаем
-			cout << "(return) - ";
+			cout << "уже есть - ";
 			return it->second;
 		}
 	}
